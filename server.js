36c198b499ab28c4d1e24c66dd5f8529e0d9d8b4
Matt <mattg317@gmail.com>
2016-10-29 11:45:36 -0400

    updated server

--- server.js
+++ server.js
@@ -1,7 +1,40 @@
-var express = require('express')
+var express = require('express');
+var bodyParser = require('body-parser');
 var app= express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 var PORT = 3000;
+
+app.use(express.static('public'));
+
+var ressies=[];
+
+app.post('/api/tables', function(req, res){
+	var resev = req.body();
+	ressies.push(resev);
+	res.json(resev);
+});
+
+app.get('/api/tables', function(req, res){
+	// var ressies= [
+	// {
+	// 	customerName: "matt",
+	// 	phoneNumber: "777",
+	// 	customerEmail: "th@",
+	// 	customerID: '44'
+	// }];
+	res.json(ressies);
+});
+
+app.get('')
+
+
+
+
 app.listen(PORT, function(){
 	console.log('listening on PORT:', PORT);
 })
